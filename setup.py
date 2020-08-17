2f5e9e028f4c209947b948ae4c098d87f236a3eb
kremazar <kmazar222@gmail.com>
2020-08-17 02:20:14 +0200

    requirements

--- setup.py
+++
@@ -1,7 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:''@localhost/web'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
