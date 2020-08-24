592ae365ab80205599a7a90af4461b5e4f531ac3
kremazar <kmazar222@gmail.com>
2020-08-24 19:38:38 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    DATABASE_URL = 'postgres://dlavmtxtspfvyi:57d4109bdc70b283acdf1183fb967a0c717e2719bb9fa83aa900f79ccf293fbf@ec2-46-137-156-205.eu-west-1.compute.amazonaws.com:5432/deepag3mr5vvva'
+    SQLALCHEMY_DATABASE_URI = 'postgres://dlavmtxtspfvyi:57d4109bdc70b283acdf1183fb967a0c717e2719bb9fa83aa900f79ccf293fbf@ec2-46-137-156-205.eu-west-1.compute.amazonaws.com:5432/deepag3mr5vvva'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
